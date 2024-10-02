ea825f9f20079c8f89ef0e08ed1c7cb1ea2091be
Anmol Shah <anmolsha80@gmail.com>
2024-10-02 20:57:34 +0545

    Create individual components for each task

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import '@/App.css';
+import Title from '@/components/Title';
+import Count from '@/components/Count';
+import ResetButton from '@/components/ResetButton';
+import CountButtons from '@/components/CountButtons';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <main>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Title />
+        <Count />
+        <ResetButton />
+        <CountButtons />
       </div>
-      <h1>Fancy Counter</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   );
 }
 
